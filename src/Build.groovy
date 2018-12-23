2de0028a1edf4c7517ef6c418629c02a0c34f95b
ominging <ominging@sina.com>
2018-12-23 14:51:13 +0800

    Update Build.groovy

--- src/Build.groovy
+++ src/Build.groovy
@@ -1,1 +1,9 @@
-test
+package com.test
+class workLabrary {
+  def print(work){
+     print "---------this it test----------work";
+  }
+   def week(work){
+     print "---------this it week----------work";
+  }
+}
