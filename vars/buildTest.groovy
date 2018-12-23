6faf2c9e221250cb492312578c0fda084aae6a34
ominging <ominging@sina.com>
2018-12-23 14:23:09 +0800

    Create buildTest.groovy

---
+++ vars/buildTest.groovy
@@ -0,0 +1,9 @@
+package com.test
+class workLabrary {
+  def print(work){
+     print "---------this it test----------work";
+  }
+   def week(work){
+     print "---------this it week----------work";
+  }
+}
