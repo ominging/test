a7589f86ee2b310ae380307e32cc1bdfc14bdc10
ominging <ominging@sina.com>
2018-12-23 16:01:41 +0800

    Update Build.groovy

--- src/org/foo/Build.groovy
+++ src/org/foo/Build.groovy
@@ -1,8 +1,6 @@
 package org.foo
-class Utilities implements Serializable {
-  def steps
-  Utilities(steps) {this.steps = steps}
-  def mvn(args) {
-    print "---------this it week----------work";
+class Utilities {
+  static def mvn(args) {
+     print "---------this it week----------work";
   }
 }
