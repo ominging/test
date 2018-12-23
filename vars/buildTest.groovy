8caa01ffde822801dbbfa53cbaf9e091b47d67f1
ominging <ominging@sina.com>
2018-12-23 14:50:12 +0800

    Delete buildTest.groovy

--- vars/buildTest.groovy
+++
@@ -1,9 +0,0 @@
-package com.test
-class workLabrary {
-  def print(work){
-     print "---------this it test----------work";
-  }
-   def week(work){
-     print "---------this it week----------work";
-  }
-}
